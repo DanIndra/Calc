1628e1f5a26f194961a955ad991f4f2099bcbafc
Daniel Mota <DanIndra@indra.co.uk>
2018-12-13 14:32:39 +0000

    update to docker

--- exportbuild.sh
+++ exportbuild.sh
@@ -1,2 +1,2 @@
 #Executable script to export the .tar file with the necessary files for the app
-docker run -p 9090 calculator tar -cvzf build.tar.gz /App/buildinfo.txt /App/Calculator
+docker run -p 9090 calculator tar -cvzf build.tar.gz ./App/buildinfo.txt ./App/Calculator
